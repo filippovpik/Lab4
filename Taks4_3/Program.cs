c38afcc972f0f81223eb0ab0f0bcd5997c6a0da4
filippovpik@outlook.com <filippovpik@outlook.com>
2025-03-02 14:05:38 +0300

    fix

---
+++ Taks4_3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Taks4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите целое число a>0");
+            int a = Convert.ToInt32(Console.ReadLine()); //input a
+
+            Console.WriteLine("Введите целое число b>0");
+            int b = Convert.ToInt32(Console.ReadLine()); //input b
+
+            int res = 1;
+
+            for (int i = 0; i <b ; i++)
+
+            {
+                res = res * a;
+            }
+
+            Console.WriteLine(res);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taks4_5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Taks4_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число в диапазоне [20;60]");
+            int a = Convert.ToInt32(Console.ReadLine()); //input a
+
+
+            while (a < 20 || a > 60)
+
+            {
+                Console.WriteLine("Введите число в диапазоне [20;60]");
+                a = Convert.ToInt32(Console.ReadLine()); //input a
+            }
+
+
+
+            Console.WriteLine("Ok");
+
+            Console.ReadKey();
+        }
+    }
+}
